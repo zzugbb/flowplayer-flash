2120c50cf1b66f987b0d8fd667b05ee41f04f069
高炳炳 <1019178691@qq.com>
2018-08-31 17:32:20 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # flowplayer-flash
+
 flowplayer-flash版播放器测试
+
+## 使用
+
+1. git clone https://github.com/zzugbb/flowplayer-flash.git
+2. cd flowplayer-flash
+3. npm install
+4. npm run start
+5. 访问 http://localhost:3001/  播放器参数测试
+6. 访问 http://localhost:3001/api  播放器api测试
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/api', function(req, res, next) {
+  res.render('api', { title: 'api 测试' });
+});
+
+module.exports = router;
